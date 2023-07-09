6d9f8f9f3e9007084c4216bc97442483cff446ab
AgenceRevontuli <tristan@revontuli.fr>
2023-07-09 09:10:03 +0200

    restore planet

--- src/script.js
+++ src/script.js
@@ -1,44 +1,75 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 /**
- * Canvas
+ * Base
  */
 
-const canvas = document.querySelector('.webgl')
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-/**
- * Scene
- */
-
+// Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
-
+// Object
 const planet = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshBasicMaterial({ color: 'red' })
+    new THREE.SphereGeometry(1, 64, 64),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
 scene.add(planet)
 
-/**
- * Camera
- */
-
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 1000)
+// Camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 100)
 camera.position.z = 3
+camera.lookAt(planet.position)
 scene.add(camera)
 
-/**
- * Renderer 
- */
+// Controls 
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Resize Handler 
+window.addEventListener('resize', () => {
+    // Update Sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update Camera 
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update Renderer 
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update Controls 
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
