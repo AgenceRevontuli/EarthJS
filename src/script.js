c670be2d686d0d1bbda409718a948e9ca0ed010f
AgenceRevontuli <tristan@revontuli.fr>
2023-07-09 11:21:04 +0200

    Add moon and animation rotation around earth

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import * as LIL from 'lil-gui'
 
 /**
@@ -14,7 +13,9 @@ const textureLoader = new THREE.TextureLoader()
 const earthTexture = textureLoader.load('/static/textures/earthmap.jpg')
 const cloudAlphaTexture = textureLoader.load('/static/textures/cloudalpha.jpg')
 const earthHeightTexture = textureLoader.load('/static/textures/earthbump.jpg')
-const oceanMap = textureLoader.load('/textures/static/earthspec.jpg')
+// Moon Texture 
+const moonTexture = textureLoader.load('/static/textures/moon.jpg')
+const moonBumpTexture = textureLoader.load('/static/textures/moonbump.jpg')
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -28,7 +29,19 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene()
 
-// Object
+// Objects 
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(0.1, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: moonTexture, 
+        bumpMap: moonBumpTexture,
+        bumpScale: 0.005
+    })
+)
+moon.position.x = 1.5
+moon.position.y = 0.5
+
 const cloud = new THREE.Mesh(
     new THREE.SphereGeometry(1.002, 64, 64),
     new THREE.MeshBasicMaterial({
@@ -47,7 +60,7 @@ const planet = new THREE.Mesh(
 )
 planet.rotation.y = -1.5
 planet.rotation.x = 0.2
-scene.add(planet, cloud)
+scene.add(planet, cloud, moon)
 
 // Lights 
 const ambientLight = new THREE.PointLight('#fff', 0.1, 100)
@@ -69,19 +82,24 @@ scene.add(ambientLightHelper)
 gui.add(ambientLight.position, 'x').min(-25).max(10).step(0.5)
 gui.add(ambientLight.position, 'y').min(-15).max(10).step(0.5)
 gui.add(ambientLight.position, 'z').min(-15).max(10).step(0.5)
-gui.add(planet.rotation, 'z').min(-15).max(10).step(0.1)
-gui.add(planet.rotation, 'x').min(-15).max(10).step(0.1)
-gui.add(planet.rotation, 'y').min(-15).max(10).step(0.1)
+const planetFolder = gui.addFolder('Planet')
+planetFolder.add(planet.rotation, 'z').min(-15).max(10).step(0.1)
+planetFolder.add(planet.rotation, 'x').min(-15).max(10).step(0.1)
+planetFolder.add(planet.rotation, 'y').min(-15).max(10).step(0.1)
+const moonFolder = gui.addFolder('Moon')
+moonFolder.add(moon.position, 'y').min(-15).max(10).step(0.1)
+moonFolder.add(moon.position, 'z').min(-15).max(10).step(0.1)
+moonFolder.add(moon.position, 'x').min(-15).max(10).step(0.1)
+moonFolder.add(moon.position, 'x').min(-15).max(10).step(0.1)
+moonFolder.add(moon.rotation, 'x').min(-15).max(10).step(0.1)
+moonFolder.add(moon.rotation, 'y').min(-15).max(10).step(0.1)
+moonFolder.add(moon.rotation, 'z').min(-15).max(10).step(0.1)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.5, 100)
 camera.position.z = 3
 camera.lookAt(planet.position)
 scene.add(camera)
-
-// Controls 
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -118,8 +136,9 @@ const tick = () =>
     cloud.rotation.x = - Math.sin(elapsedTime * 0.003) 
     cloud.rotation.y = - Math.cos(elapsedTime * 0.003) 
 
-    // Update Controls 
-    controls.update()
+    // Animate Moon 
+    moon.position.x = - (Math.cos(elapsedTime * 0.03)) * 1.5
+    moon.position.z = - (Math.sin(elapsedTime * 0.03)) * 1.5
 
     // Render
     renderer.render(scene, camera)
